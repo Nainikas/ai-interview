a5c3e2c72c51f493804753ebbe698a89fa28a044
Nainikas <55590634+Nainikas@users.noreply.github.com>
2025-07-14 21:06:04 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@mediapipe/camera_utils',
-        '@mediapipe/face_mesh',
-        '@mediapipe/drawing_utils'
-      ],
-    },
+  optimizeDeps: {
+    include: [
+      '@mediapipe/face_mesh',
+      '@mediapipe/camera_utils',
+      '@mediapipe/drawing_utils'
+    ]
   },
+  build: {
+    commonjsOptions: {
+      // ensure any CommonJS mediapipe helpers get picked up
+      include: [/node_modules/, /@mediapipe/]
+    }
+  }
 });
